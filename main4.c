f43ec574c8f4b951719e481d184befc64e02ca0a
qa876079-eng <qa876079@gmail.com>
2026-06-09 16:02:38 +0530

    Implement absolute value calculation in main4.c

---
+++ main4.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Write C code here
+    float x, y;
+    printf(" Enter any number:");
+    scanf("%f", &x);
+    y = abs(x);
+    printf("absolute value of x is:%f", y);
+    return 0;
+}
